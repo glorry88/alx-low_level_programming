4afecdf8d799b09fc4a56863e60accc635e7947d
gloryy88 <aglagosamuellamanye@gmail.com>
2021-06-17 18:57:18 +0000

    Use strings

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /*
- *main - prints a string to the standard output
- *Return:1
- *Description: printing to standard output
+ * main - prints a string to the standard output
+ * Return:1
+ * Description: printing to standard output
  */
 int main(void)
 {
-	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+    char[] string = "and that piece of art is useful\"";
+    char[] author =  " - Dora Korpar, 2015-10-19\n";
+	fprintf(stderr, "%s", strcat(string, author) );
 	return (1);
 }
