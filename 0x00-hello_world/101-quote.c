99d870cc15040133d769aa3c22890bd77b2ab828
glorry88 <aglagosamuellamanye@gmail.com>
2021-06-17 11:50:53 -0700

    over 80 characters

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/*
+/**
  *main - prints a string to the standard output
  *Return:1
  *Description: printing to standard output
  */
-int main(void){
+int main(void)
+{
 	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
