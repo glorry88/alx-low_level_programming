76baf4f1e0825dbf7bf321838bfa1e2257c70e2f
gloryy88 <aglagosamuellamanye@gmail.com>
2021-06-17 17:46:29 +0000

    standard output

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,9 @@
 /*
 *main - prints a string to the standard output
 *Return:1
-**/
+*
+*Description: printing to standard output
+*/
 int main(void)
 {
 	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
