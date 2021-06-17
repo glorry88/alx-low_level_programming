7ff7e64d1f7fb222c2b8d28b2bbbc1ceead5dbd5
gloryy88 <aglagosamuellamanye@gmail.com>
2021-06-17 17:01:23 +0000

    standard output

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+/**
+ * main - prints a string to the standard output
+ * Return: 1 
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
