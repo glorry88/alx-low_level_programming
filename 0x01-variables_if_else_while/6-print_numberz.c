73d5bc2e6ce74d9ec80458265d8b6e9106c55d51
glorry88 <aglagosamuellamanye@gmail.com>
2021-06-18 11:21:42 -0700

    printing single digits with putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
- * main - prints alphabets without q and e
- * Return: Always 0 (Success)
+ * main - Prints all single digit numbers of base 10 starting from 0,
+ *        only using putchar and without char variables.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-int  num = '0';
-while (num <= '9')
-{
-putchar (num);
-num++;
-}
-printf("\n");
+int num;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+putchar('\n');
 return (0);
 }
-
