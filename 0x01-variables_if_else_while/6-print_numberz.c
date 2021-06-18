fd4d6318d1df65248ca7bd0ed1030727c4f104ef
glorry88 <aglagosamuellamanye@gmail.com>
2021-06-18 10:45:20 -0700

    printing numbers between 0 and 10 using putchar function

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints alphabets without q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char num = '0';
+while (num <= '9')
+{
+putchar (num);
+num++;
+}
+printf("\n");
+return (0);
+}
+
