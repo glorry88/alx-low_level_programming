2774d4d1e840f590240dd4e216f61867e4daa91b
glorry88 <aglagosamuellamanye@gmail.com>
2021-06-18 12:00:01 -0700

    printing hexedecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - printing hexadecimals
+ * using only putchar
+ * Return: always  0 (success)
+ */
+int main(void)
+{
+int alpha;
+char lower;
+for (alpha = 0; alpha < 10; alpha++)
+{
+putchar((alpha % 10) + '0');
+}
+for (lower = 97; lower < 103; lower++)
+{
+putchar(lower);
+}
+putchar('\n');
+}
