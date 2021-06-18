457055fee0bc2dc00229209756784bcf25cb0c38
glorry88 <aglagosamuellamanye@gmail.com>
2021-06-18 10:14:32 -0700

    printing to skip some alphabets

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,11 +3,22 @@
 #include <stdio.h>
 
 /**
- * main - prints alphabets without q and e
+ * main - prints alphabet s without q and e
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("abcdfghijklmnoprstuvwxyz\n");
+char alpha = 97;
+while (alpha <= 122)
+{
+if (alpha == 101 || alpha == 113)
+{
+alpha++;
+continue;
+}
+putchar(alpha);
+alpha++;
+}
+printf("\n");
 return (0);
 }
