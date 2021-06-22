f71e7b4cfeacbe5507ff0bae5acfc695762443a6
glorry88 <aglagosamuellamanye@gmail.com>
2021-06-22 05:24:29 -0700

    using a loop to print a string

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -5,15 +5,12 @@
  */
 int main(void)
 {
-_putchar('H');
-_putchar('o');
-_putchar('l');
-_putchar('b');
-_putchar('e');
-_putchar('r');
-_putchar('t');
-_putchar('o');
-_putchar('n');
+char holberton[9] = "Holberton";
+int i;
+for (i = 0; i < 9; i++)
+{
+_putchar(holberton[i]);
+}
 _putchar('\n');
 return (0);
 }
