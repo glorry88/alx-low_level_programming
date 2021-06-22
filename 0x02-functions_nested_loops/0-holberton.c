f1b53c5ad4a5366fd2e59ef01aaf1d69a37c3c9b
gloryy88 <aglagosamuellamanye@gmail.com>
2021-06-22 11:03:23 +0000

    printing holberton using putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include <iostream>
+/**
+ * main - printing a string using putchar
+ * Return: always 0 (success)
+ */
+int main(){
+putchar('H');
+putchar('o');
+putchar('l');
+putchar('b');
+putchar('e');
+putchar('r');
+putchar('t');
+putchar('o');
+putchar('n');
+putchar('\n');
+return 0;
+}
