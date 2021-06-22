9aaf5aefc3093cee7937f711b2077b9432ce94a0
gloryy88 <aglagosamuellamanye@gmail.com>
2021-06-22 12:50:40 +0000

    definining holberton.h file

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,12 +1,8 @@
-#include <unistd.h>
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
+#ifndef HOLBERTON_HEADER
+#define HOLBERTON_HEADER
+extern int _putchar(char c);
+extern void print_alphabet(void);
+extern void print_alphabet_x10(void);
+extern int _islower(int c);
+extern int _isalpha(int c);
+#endif
