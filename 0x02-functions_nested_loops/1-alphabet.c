a034e75f980800e7b20b04275595968faf95dabd
gloryy88 <aglagosamuellamanye@gmail.com>
2021-06-22 13:07:40 +0000

    printin alphabets with prototypes

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/** 
+ * main - using function prototypes
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+for (int i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+return (0);
+}
+ 
