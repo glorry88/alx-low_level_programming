a0bdc89d59cfa322f235ef4734721beb2ff2f221
gloryy88 <aglagosamuellamanye@gmail.com>
2021-06-24 17:11:01 +0000

    function to print straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * void print_line - function that draws straight line in terminal
+ * Return: straight line
+ *@n: number to be checked
+ */
+void print_line(int n){
+if (n<0){
+_putchar('\n');
+}
+else {
+int i;
+for (i = 0; i <= n; i++)
+{
+putchar('_');
+}
+}
+_putchar('\n');
+}
