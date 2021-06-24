7c42d49a54739834bc1426df1eaccea7dd654f81
gloryy88 <aglagosamuellamanye@gmail.com>
2021-06-24 18:58:48 +0000

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *
+ *
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 != 0){
+printf("%s", "Fizz ");
+continue;
+}
+else if (i % 5 == 0 && i % 3 != 0){
+printf("%s", "Buzz ");
+continue;
+}		
+else if (i % 3 == 0 && i % 5 == 0){
+printf("%s", "FizzBuzz ");
+continue;
+}		
+printf("%d",i);
+printf(" ");
+}
+putchar('\n');
+return 0;
+}
