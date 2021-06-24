2fc635447d6cde2837e94e769dc47861118a2ced
gloryy88 <aglagosamuellamanye@gmail.com>
2021-06-24 18:29:26 +0000

    function to print a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,6 +6,12 @@
  **/
 void print_square(int size)
 {
+if (size <= 0)
+{
+_putchar('\n');
+}
+else 
+{
 int count, i;
 for (count = 0; count < size; count++)
 {
@@ -14,6 +20,7 @@ for (i = 0; i < size; i++)
 _putchar('#');
 }
 _putchar('\n');
+}
 }
 _putchar('\n');
 }
