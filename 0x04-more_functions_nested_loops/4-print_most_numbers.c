ffbef2b4f5ce135ebaaafd5178659cb85554860c
gloryy88 <aglagosamuellamanye@gmail.com>
2021-06-24 08:57:55 +0000

    print numbers and skip some

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton"
+/** 
+ * void print_most_numbers(void) - prints numbers and skip some
+ * Return: 0 always
+ */
+void print_most_numbers(void)
+{
+    int i;
+for (i = '0'; i <= '9'; i++){
+    if ( i == '2' || i == '4')
+    {
+        continue;
+    }
+_putchar(i);
+}
+_putchar('\n');
+}
