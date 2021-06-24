49a57801eaf0cb2288df549866e9682ba5f80732
gloryy88 <aglagosamuellamanye@gmail.com>
2021-06-24 08:36:54 +0000

    multiplying two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,8 @@
+#include "holberton"
+/**
+ * int mul(int a, int b) - multiplies
+ *
+ */
+int mul(int a, int b){
+    return ( a * b);
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -2,4 +2,5 @@
 #define HOLBERTON_HEADER
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 #endif
