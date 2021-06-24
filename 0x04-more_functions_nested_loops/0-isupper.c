7429d490b95e65016fe383e6feefc2760a66bf92
gloryy88 <aglagosamuellamanye@gmail.com>
2021-06-24 08:20:09 +0000

    return uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * int _isupper(int c) - checking upper case
+ * Return: 1 if c is uppercase and 0 if otherwise
+ */
+
+ int _isupper(int c){
+     return (c >= 'A' && c <= 'Z');
+ }
+
