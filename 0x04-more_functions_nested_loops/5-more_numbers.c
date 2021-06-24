c5fe1666e846e608be1a3da66b28b251c7fff8c4
gloryy88 <aglagosamuellamanye@gmail.com>
2021-06-24 16:25:55 +0000

    function that prints numbers many times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * more_numbers - function to print 10 times numbers
+ * Return: multiple numbers
+ *
+ */
+void more_numbers(void)
+{
+int i, j;
+for (i = 0; i <= 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+_putchar(j);
+}
+_putchar(i);
+}
+_putchar('\n');
+ }
