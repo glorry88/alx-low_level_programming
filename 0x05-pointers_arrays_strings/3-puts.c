77baabaa8836919f88d3b7fdee7ecf506bea5960
gloryy88 <aglagosamuellamanye@gmail.com>
2021-06-27 23:18:50 +0000

    function to print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _strlen - function to print to print a string
+ * @s: string parameter
+ * Return: string followed by new line
+ */
+int _strlen(char *s)
+{
+int i;
+for (i = 0; *s !='\0'; i++ )
+{
+_putchar(*s[i]);
+}
+_putchar('\n');
+}
