dbeb76901071b24811eadf4a8065572420868758
gloryy88 <aglagosamuellamanye@gmail.com>
2021-06-27 22:57:03 +0000

    making changes to file 0 and working on file 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "holbeton.h"
 /**
  * swap_int - function to swap two integers
  * @a: first number to be checked
@@ -9,6 +9,4 @@ void swap_int(int *a, int *b)
 {
 *a = *b;
 *b = *a;
-_putchar(*a);
-_putchar(*b);
 }
