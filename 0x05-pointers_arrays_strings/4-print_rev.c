23d654312f6d8d8b5b80f7214526127f68f063d5
gloryy88 <aglagosamuellamanye@gmail.com>
2021-06-27 23:30:04 +0000

    fixing file 3 and printing string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_rev - print string in reverse
+ * @s: parameter
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+    int i;
+for (i = 0; s[i] < '\0'; i++)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
