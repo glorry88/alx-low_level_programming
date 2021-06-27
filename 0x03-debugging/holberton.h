ec68ad88574f0187b36b985aaec3c7e15dcc3b5c
gloryy88 <aglagosamuellamanye@gmail.com>
2021-06-27 22:10:47 +0000

    resetting file 0

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,5 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+int convert_day(int day, int month);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,4 @@
+#ifndef HOLBERTON_HEADER
+#define HOLBERTON_HEADER
+void reset_to_98(int *n);
+#endif
