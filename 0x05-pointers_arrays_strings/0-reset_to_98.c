ac7c25f85ad129061df29cbc0128fadeae944898
gloryy88 <aglagosamuellamanye@gmail.com>
2021-06-27 22:12:19 +0000

    resetting file 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,10 @@
 /**
  * reset_to_98 - taking a pointer to int as parameter
  * and reseting the value to 98
+ * @n: parameter to be checked
  * Return: 0 always
  */
- void reset_to_98(int *n)
+void reset_to_98(int *n)
 {
 return (*n = 98);
 }
