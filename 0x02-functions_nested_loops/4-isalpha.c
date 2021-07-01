502315cba5c85c7c03e0d12dbc05dbc3cdb78549
gloryy88 <aglagosamuellamanye@gmail.com>
2021-07-01 20:09:00 +0000

    seen checker works, now inputing my codes

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
  * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
-{		
+{
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
 return (1);
@@ -13,5 +13,5 @@ return (1);
 else
 {
 return (0);
-}			
+}
 }
