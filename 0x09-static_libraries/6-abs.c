35839303615801cae6f369155d7e333367717872
gloryy88 <aglagosamuellamanye@gmail.com>
2021-07-13 17:04:03 +0000

    new

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int c)
+{
+if (c < 0)
+{
+return (-c);
+}
+else
+{
+return (c);
+}		
+}
