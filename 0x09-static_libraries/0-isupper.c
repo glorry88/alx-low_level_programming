c756b15af6fd65550e69b65d42402a65f6ecb987
gloryy88 <aglagosamuellamanye@gmail.com>
2021-07-13 13:20:05 +0000

    copying files

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * _isupper - checking upper case
+ *@c: the character to be checked
+ * Return: 1 if c is uppercase and 0 if otherwise
+ */
+
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x09-static_libraries/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * mul - multiplies
+ * @a: first number to be checked
+ * @b: second number to be checked
+ * Return: results of multiplication
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
