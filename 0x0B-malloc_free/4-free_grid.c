4a14baad025b607f969a26ba76b31ef35bb94620
gloryy88 <aglagosamuellamanye@gmail.com>
2021-07-15 15:09:48 +0000

    free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * free_grid - function
+ * @grid: parameter
+ * @height: parameter
+ */
+
+void free_grid(int **grid, int height)
+{
+int i;
+for (i = 0; i < height; i++)
+{
+free(grid[i]);
+}
+free(grid);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -2,4 +2,6 @@
 #define HOLBERTON_HEADER
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+void free_grid(int **grid, int height);
 #endif
